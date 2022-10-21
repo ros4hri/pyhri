29fe953de89deebb3a0b7319107a7916e37df5b5
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-10-21 09:32:13 +0900

    voice: expose last recognised speech + transform

--- src/hri/voice.py
+++ src/hri/voice.py
@@ -26,8 +26,16 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from typing import Optional
+
 try:
     import rospy
+    from hri_msgs.msg import LiveSpeech
+    from geometry_msgs.msg import TransformStamped
+
+    from tf2_ros import LookupException
+
+    VOICE_TF_TIMEOUT = rospy.Duration(0.01)
 except ImportError:
     print(
         "Importing pyhri without rospy! This won't work (except for generating documentation)"
@@ -38,24 +46,73 @@ class Voice:
     """Represents a detected voice."""
 
     def __init__(self, id, tf_buffer, reference_frame):
-        self.id = id
-        self.ns = "/humans/voices/" + id
+        self.id = id  #: the voice ID
+        self.ns = "/humans/voices/" + id  #: the full namespace of the voice
+        self.frame = "voice_" + id  #: the voice tf frame name
 
         self._valid = True
 
+        self._tf_buffer = tf_buffer
+        self._reference_frame = reference_frame
+
+        self.incremental_speech: Optional[
+            str
+        ] = None  #: last incrementally recognised speech -- that string might entirely change, based on the what the ASR recognised
+
+        self.speech: Optional[str] = None  #: last recognised speech, if available
+
         rospy.logdebug("New voice detected: " + self.ns)
+
+        self._speech_sub = rospy.Subscriber(
+            self.ns + "/speech", LiveSpeech, self._on_speech
+        )
 
     def close(self):
         self._valid = False
 
+    @property
     def valid(self) -> bool:
-        """Returns True if this voice still exists (and thus is valid).
-        If False, methods like `Voice.transform` will raise an exception.
+        """Returns `True` if this voice still exists (and thus is valid).
+        If `False`, methods like `Voice.transform` will raise an exception.
         """
         return self._valid
 
-    def transform(self):
-        raise NotImplementedError("method not yet implemented")
+    def _on_speech(self, msg):
+        self.incremental_speech = msg.incremental
+        self.speech = msg.final
+
+    def transform(self, from_frame=None):
+        """Returns a ROS TransformStamped of the voice, from the `from_frame`
+        reference basis.
+
+        If `from_frame` is not provided, uses the default `reference_frame`
+        (usually `base_link`).
+
+        Note that the spatial location of a voice is inherently very
+        approximate. Most sound source localisation algorithm will actually
+        only return the 'DoA' (*direction of arrival*) of the voice -- in that
+        case, only the heading of the tf frame with respect to the reference
+        frame -- not the distance -- should be considered.
+        """
+
+        if from_frame is None:
+            from_frame = self._reference_frame
+
+        try:
+            return self._tf_buffer.lookup_transform(
+                from_frame, self.frame, rospy.Time(0), VOICE_TF_TIMEOUT
+            )
+
+        except LookupException:
+            rospy.logwarn(
+                "failed to transform voice frame "
+                + self.frame
+                + " to "
+                + from_frame
+                + ". Are the frames published?"
+            )
+
+            return TransformStamped()
 
     def __str__(self):
         return self.id
